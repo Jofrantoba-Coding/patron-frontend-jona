83b3eb7f2914180db827fe020fb69f22403e7ebb
jofrantoba <chescot2302@gmail.com>
2026-05-25 23:17:29 -0500

    delete molecule Alert

--- web_ts/uijona/src/molecules/AlertMolecule/AlertMolecule.tsx
+++
@@ -1,3 +0,0 @@
-// AlertMolecule.tsx — JONA Template (punto de entrada público)
-export { AlertMoleculeImpl as AlertMolecule } from './AlertMoleculeImpl';
-export type { InterAlertMolecule, AlertVariant } from './InterAlertMolecule';
